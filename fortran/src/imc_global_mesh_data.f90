dbf9d6f3cb7d7f4bc2b6ad36a7fd7bc52b8eb498
msleigh <msleigh@users.noreply.github.com>
2026-02-10 07:19:12 +0000

    enhance pre-commit configuration and clean up Fortran source files
    - Added additional pre-commit hooks: check-case-conflict, check-merge-conflict, check-symlinks, check-toml, and mixed-line-ending.
    - Removed unnecessary blank lines in Fortran source files.
    - Ensured a newline at the end of the LICENSE.txt file.

--- fortran/src/imc_global_mesh_data.f90
+++ fortran/src/imc_global_mesh_data.f90
@@ -1,11 +1,11 @@
 module imc_global_mesh_data
 
     implicit none
-    
+
     real(kind=8) :: xsize  = -1.d0
     real(kind=8) :: dx     = -1.d0
     integer      :: ncells = -1
-    
+
     real(kind=8), allocatable, dimension(:) :: cellpos
     real(kind=8), allocatable, dimension(:) :: nodepos
     real(kind=8), allocatable, dimension(:) :: sigma_p
@@ -14,5 +14,5 @@ module imc_global_mesh_data
     real(kind=8), allocatable, dimension(:) :: beta
     real(kind=8), allocatable, dimension(:) :: fleck
     real(kind=8), allocatable, dimension(:) :: nrgdep
-    
+
 end module imc_global_mesh_data
